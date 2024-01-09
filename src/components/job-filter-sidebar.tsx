28a696fa05e3d56be058a9f0c89c76b3a49b18c8
Jerome Doran <disneyfan9184@gmail.com>
2024-01-09 16:15:17 -0500

    added filter form ui

--- src/components/job-filter-sidebar.tsx
+++ src/components/job-filter-sidebar.tsx
@@ -1,12 +1,34 @@
+import prisma from "@/lib/prisma";
+
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import Select from "./ui/select";
+import { jobTypes } from "@/lib/job-types";
+import { Button } from "./ui/button";
 
 async function filterJobs(formData: FormData) {
   "use server";
+  console.log(formData.get("q"));
 }
 
-export default function JobFilterSidebar() {
+export default async function JobFilterSidebar() {
+  const distinctLocations = (await prisma.job
+    .findMany({
+      where: {
+        approved: true,
+      },
+      select: {
+        location: true,
+      },
+      distinct: ["location"],
+      orderBy: {
+        location: "asc",
+      },
+    })
+    .then(
+      (locations) => locations.map(({ location }) => location).filter(Boolean), // filter out empty strings with Boolean
+    )) as string[];
+
   return (
     <aside className="sticky top-0 h-fit rounded-lg border bg-background p-4 md:w-[260px]">
       <form action={filterJobs}>
@@ -16,9 +38,39 @@ export default function JobFilterSidebar() {
             <Input id="q" name="q" placeholder="Title, company, etc." />
           </div>
           <div className="flex flex-col gap-2">
-            <Label htmlFor="location">Location</Label>
-            <Select id="location" name="location" />
+            <Label htmlFor="type">Type</Label>
+            <Select id="type" name="type" defaultValue="">
+              <option value="">All types</option>
+              {jobTypes.map((type) => (
+                <option key={type} value={type}>
+                  {type}
+                </option>
+              ))}
+            </Select>
           </div>
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="location">Location</Label>
+            <Select id="location" name="location" defaultValue="">
+              <option value="">All locations</option>
+              {distinctLocations.map((location) => (
+                <option key={location} value={location}>
+                  {location}
+                </option>
+              ))}
+            </Select>
+          </div>
+          <div className="flex items-center gap-2">
+            <input
+              id="remote"
+              name="remote"
+              type="checkbox"
+              className="scale-125 accent-black"
+            />
+            <Label htmlFor="remote">Remote jobs</Label>
+          </div>
+          <Button type="submit" className="w-full">
+            Filter Jobs
+          </Button>
         </div>
       </form>
     </aside>
---
+++ src/lib/job-types.ts
@@ -0,0 +1,10 @@
+export const jobTypes = [
+  "Full-time",
+  "Part-time",
+  "Contract",
+  "Internship",
+  "Temporary",
+  "Volunteer",
+] as const;
+
+export const locationTypes = ["Remote", "Hybrid", "On-site"] as const;
